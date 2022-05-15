743604e7d0ae6ffb5bf032919c79ef2424cffba4
Jean Haro <JeanCHL98@hotmail.com>
2022-05-15 03:26:52 -0500

    Login estructurado y estilizado

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Iconos - FontAwesome
+import {
+  faEnvelope,
+  faLock,
+  faCheck
+} from '@fortawesome/free-solid-svg-icons';
+
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +18,11 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  // Iconos
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+  faCheck = faCheck;
+  faGooglePlus = faGooglePlus;
 }
