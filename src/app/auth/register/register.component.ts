db37ed975a7814aee76d516d69958d41b44027c3
Jean Haro <JeanCHL98@hotmail.com>
2022-05-15 18:04:43 -0500

    Register estructurado y estilizado

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// Iconos - FontAwesome
+import {
+  faEnvelope,
+  faLock,
+  faCheck,
+  faUser
+} from '@fortawesome/free-solid-svg-icons';
+
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -12,4 +22,10 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Iconos
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+  faCheck = faCheck;
+  faGooglePlus = faGooglePlus;
+  faUser = faUser;
 }
