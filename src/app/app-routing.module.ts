31b825632fb10ba54f37d9df51336defb6bbb676
Jean Haro <JeanCHL98@hotmail.com>
2022-05-28 20:28:06 -0500

    Colocando rutas para el auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Componentes - Auth
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+// Routing
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 // Componente - 404
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
-import { PagesRoutingModule } from './pages/pages.routing';
+
 
 // Rutas de aplicación
 const routes: Routes = [
-  // Auth
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
   // No hay o no existe la ruta
   { path: '**', component: NoPageFoundComponent }
 ];
@@ -22,7 +18,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
---
+++ src/app/auth/auth.routing.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+// Componentes - Auth
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class AuthRoutingModule {}
